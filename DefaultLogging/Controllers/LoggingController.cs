6a3b306417d4d5fc07082756723dcdc3a6f092ba
Balaji Singh <khbalaji21@gmail.com>
2023-06-21 11:06:05 +0530

    Add project files.

---
+++ DefaultLogging/Controllers/LoggingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DefaultLogging.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoggingController : ControllerBase
+    {
+        private readonly ILogger<LoggingController> _logger;
+
+        public LoggingController(ILogger<LoggingController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("Message Logged!!");
+            return "Check on the console if a message is logged";
+        }
+    }
+}
